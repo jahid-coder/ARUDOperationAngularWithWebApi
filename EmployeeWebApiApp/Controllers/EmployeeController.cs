1a1c5c5cacdbd0d5f12b025c07dd5f4d65938ff3
jahidul Islam <jahidiu@gmail.com>
2022-03-21 22:03:05 +0600

    api

---
+++ EmployeeWebApiApp/Controllers/EmployeeController.cs
@@ -0,0 +1,144 @@
+﻿using EmployeeWebApiApp.Data;
+using EmployeeWebApiApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+
+        public EmployeeController(UserDbContext userDbContext)
+        {
+            _context = userDbContext;
+        }
+
+        [HttpPost("add_employee")]
+        public IActionResult AddEmployee([FromBody] EmployeeModel employeeObj )
+        {
+            if(employeeObj == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                _context.employeeModel.Add(employeeObj);
+                _context.SaveChanges();
+                return Ok(new
+                {
+                    StatusCode = 200,
+                    Message = "Employee Add Successfully "
+                });
+            }
+
+        }
+
+        [HttpPut("update_employee")]
+        public IActionResult UpdateEmployee([FromBody] EmployeeModel employeeObj)
+        {
+            if (employeeObj== null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                var user = _context.employeeModel.AsNoTracking().FirstOrDefault(x => x.Id == employeeObj.Id);
+
+                if(user == null)
+                {
+                    return NotFound(new
+                    {
+                        StatusCode = 404,
+                        Message = "User not fond",
+                    });
+                }
+                else
+                {
+                    _context.Entry(employeeObj).State = EntityState.Modified;
+                    _context.SaveChanges();
+                    return Ok(new
+                    {
+                        StatusCode = 200,
+                        Message = "Employee Updated Successfully"
+                    });
+                 }
+                
+            }
+        }
+
+        [HttpDelete("delete_employee/{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            var user = _context.employeeModel.Find(id);
+            if(user == null)
+            {
+                return NotFound(new
+                {
+                    StatusCode = 404,
+                    Message = "User not found",
+                });
+            }
+            else
+            {
+                _context.employeeModel.Remove(user);
+                _context.SaveChanges();
+                return Ok(new
+                {
+                    status = 200,
+                    Message = "Employee is deleted Sucessfully",
+                });
+
+
+            }
+        }
+
+        [HttpGet("get_all_employees")]
+
+        public IActionResult GetAllEmployees()
+        {
+            var employees = _context.employeeModel.AsQueryable();
+
+            return Ok(new
+            {
+                StatusCode = 200,
+                EmployeeDetails = employees,
+            }); 
+
+        }
+
+        [HttpGet("get_employee/id")]
+
+        public IActionResult GetEmployee(int id)
+        {
+            var employee = _context.employeeModel.Find(id);
+            if(employee == null)
+            {
+                return NotFound(new
+                {
+                    StatusCode = 404,
+                    Message = "User not found"
+                });
+
+            }
+            else
+            {
+                return Ok(new
+                {
+                    StatusCode = 200,
+                    EmployeeDetail = employee,
+                });
+               
+            }
+        }
+
+
+    }
+}
---
+++ EmployeeWebApiApp/Controllers/LoginController.cs
@@ -0,0 +1,119 @@
+﻿using EmployeeWebApiApp.Data;
+using EmployeeWebApiApp.Helpers;
+using EmployeeWebApiApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+        private readonly IConfiguration _config;
+        public LoginController(UserDbContext userDbContext, IConfiguration config)
+        {
+            _context = userDbContext;
+            _config = config;
+        }
+
+        [HttpGet("users")]
+        public ActionResult GetUsers()
+        {
+            var userdetails = _context.userModel.AsQueryable();
+            return Ok(userdetails);
+        }
+
+        [HttpPost("signup")]
+        public ActionResult SignUp([FromBody] UserModel userObj)
+        {
+            if(userObj == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                userObj.Password = EncDscPassword.EncryptPassword(userObj.Password);
+                _context.userModel.Add(userObj);
+                _context.SaveChanges();
+                return Ok(new
+                {
+                    StatusCode = 200,
+                    Message = "User Added Sucessfully",
+                }) ;
+
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult LogIn([FromBody] UserModel userObj)
+        {
+            if(userObj== null)
+            {
+                return BadRequest();
+                   
+            }
+            else
+            {
+                var user = _context.userModel.Where(a =>
+                a.UserName == userObj.UserName).FirstOrDefault();
+
+
+                if (user != null && EncDscPassword.DecryptPassword(user.Password) == userObj.Password)
+                {
+                    var token = GenerateToken(user.UserName);
+                    return Ok(new
+                    {
+                        StatusCode = 200,
+                        Message = "Logged in  Sucessfully",
+                        UserDetails = user,
+                        JwtToken = token
+                    }) ;
+
+                }
+                else
+                {
+                    return NotFound(new
+                    {
+                        StatusCode = 404,
+                        Message = "User not Found"
+                    });
+                }
+
+            }
+
+        }
+
+        private string GenerateToken(string username)
+        {
+            var TokenHandler = new JwtSecurityTokenHandler();
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:key"]));
+            var crediential = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email,username),
+                new Claim("Companyname","let's program")
+            };
+            var Token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: crediential); 
+           
+            return TokenHandler.WriteToken(Token);
+        }
+
+       
+    }
+}
